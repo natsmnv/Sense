12fde5f14f4daa58eb955fbc8b1450961a04c2a2
natsmnv <natnatanatasha@gmail.com>
2024-04-24 17:39:10 +0300

    Modified form and added script for form-section (POST-request)

--- script/script.js
+++ script/script.js
@@ -157,4 +157,38 @@ document.addEventListener('DOMContentLoaded', () => {
     nextSliderBtn.addEventListener('click', () => {
         showSlide(++sliderIndex);
     });
+
+
+
+    // Form
+
+    // POST-request
+
+    const form = document.querySelector('.info__form');
+
+    const postData = async (url, data) => {
+        let res = await fetch(url, {
+            method: "POST",
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: data
+        });
+
+        return await res.json();
+    }
+
+    function bindPostData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const formData = new FormData(form);
+            const json = JSON.stringify(Object.fromEntries(formData.entries()));
+
+            postData('http://localhost:3000/requests', json)
+            .finally(form.reset());
+        });
+    }
+
+    bindPostData(form);
 });
